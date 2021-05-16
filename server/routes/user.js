aa8efe3fd82c4beb39011fe67f57ee07f0eae544
Sandy Long <sandylx33@gmail.com>
2021-05-16 12:29:20 -0400

    set up register route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,11 @@
+const config = require('config')
 const express = require('express');
 const { check, validationResult } = require('express-validator/check');
-const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
+const router = express.Router();
 // Registration Route
 router.post('/', [
     check('name', 'Name is required')
@@ -12,15 +16,55 @@ router.post('/', [
     check('password', 'Your password must be at least 8 characters, contain special characters, and have lower and upper case alphabet')
         .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
 ], async (req, res) => {
-    const errors = validationResult(req); //returns an object
+    //returns an object containing matching errors
+    const errors = validationResult(req); 
     
     if(!errors.isEmpty()){
+        // errors.array() => [{ msg: "message" }]
+        // To-Do: use TS, throw JS Errors, and use express middleware to ensure error response comes in specified format
         return res.status(400).json({ errors: errors.array() });
     };
 
     const { name, email, password } = req.body; 
 
     try {
+        //returns promise
+        let user = await User.findOne({ email });
+        if(user) {
+            return res.status(400).json({ errors: [{ msg: "User already Exists" }] })
+        }
+
+        //create user
+        user = new User({
+            name,
+            email,
+            password
+        })
+
+        //encrypt password using Bcrypt. genSalt returns promise. Salt is a random string that makes hash unpredictable
+        const salt = await bcrypt.genSalt(10)
+        user.password = await brcypt.hash(password, salt)
+
+        //save user to the database
+        await user.save();
+
+        //once user credentials are validated, the server sends back a signed token to the browser
+        //afterwards, each request coming from the browser will come along with the token
+        //app server checks JWT to identify the person. 
+        //App knows the request is coming from user because a token is only signed after successful login validation
+        const payload = {
+            user: { id: user.id }
+        };
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn: 36000},
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token })
+            }
+        );
 
     } catch (err) {
         console.error(err);
